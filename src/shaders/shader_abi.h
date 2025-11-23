acef565f68234d05554ae5ae64e5414bf2c83214
Mate Buljan <mate.buljan1@gmail.com>
2025-11-23 10:57:21 +0100

    add textures and samplers to shader abi

--- src/shaders/shader_abi.h
+++ src/shaders/shader_abi.h
@@ -7,6 +7,8 @@ static constexpr Uint32 SW_MAX_VS_INPUTS  = D3D11_IA_VERTEX_INPUT_RESOURCE_SLOT_
 static constexpr Uint32 SW_MAX_VARYINGS   = D3D11_VS_OUTPUT_REGISTER_COUNT;
 static constexpr Uint32 SW_MAX_PS_OUTPUTS = D3D11_PS_OUTPUT_REGISTER_COUNT;
 static constexpr Uint32 SW_MAX_CBUFS      = D3D11_COMMONSHADER_CONSTANT_BUFFER_API_SLOT_COUNT;
+static constexpr Uint32 SW_MAX_TEXTURES   = D3D11_COMMONSHADER_INPUT_RESOURCE_SLOT_COUNT;
+static constexpr Uint32 SW_MAX_SAMPLERS   = D3D11_COMMONSHADER_SAMPLER_SLOT_COUNT;
 
 struct SW_float4
 {
@@ -18,9 +20,34 @@ struct SW_uint3
     Uint32 x, y, z;
 };
 
+struct SW_Texture
+{
+    const void* data;
+    Uint32      width;
+    Uint32      height;
+    Uint32      depth;
+    Uint32      rowPitch;
+    Uint32      slicePitch;
+    Uint32      mipLevels;
+    DXGI_FORMAT format;
+};
+
+struct SW_Sampler
+{
+    D3D11_FILTER               filter;
+    D3D11_TEXTURE_ADDRESS_MODE addressU;
+    D3D11_TEXTURE_ADDRESS_MODE addressV;
+    D3D11_TEXTURE_ADDRESS_MODE addressW;
+    Float                      mipLODBias;
+    Float                      minLOD;
+    Float                      maxLOD;
+};
+
 struct SW_Resources
 {
     const SW_float4* cb[SW_MAX_CBUFS];
+    SW_Texture       tex[SW_MAX_TEXTURES];
+    SW_Sampler       smp[SW_MAX_SAMPLERS];
 };
 
 struct SW_VSInput
