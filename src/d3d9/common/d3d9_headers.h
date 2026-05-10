c251af3e2f7bd1b36d71bcdcf2cf19f27bea8477
Mate Buljan <mate.buljan1@gmail.com>
2026-05-10 22:02:06 +0100

    win32 missing D3DSHADER_PARAM_DSTMOD_TYPE from d3d9 header for some reason

--- src/d3d9/common/d3d9_headers.h
+++ src/d3d9/common/d3d9_headers.h
@@ -9,6 +9,22 @@
     #endif
     #include <windows.h>
     #include <d3d9.h>
+
+    // Modern Windows SDK lacks D3DSHADER_PARAM_DSTMOD_TYPE (only existed in d3d8types.h)
+    #ifndef D3DSP_DSTMOD_SHIFT
+    #define D3DSP_DSTMOD_SHIFT 20
+    #define D3DSP_DSTMOD_MASK  (0xF << D3DSP_DSTMOD_SHIFT)
+    #endif
+
+    #ifndef D3DSPDM_NONE
+    typedef enum _D3DSHADER_PARAM_DSTMOD_TYPE {
+        D3DSPDM_NONE             = 0 << D3DSP_DSTMOD_SHIFT,
+        D3DSPDM_SATURATE         = 1 << D3DSP_DSTMOD_SHIFT,
+        D3DSPDM_PARTIALPRECISION = 2 << D3DSP_DSTMOD_SHIFT,
+        D3DSPDM_MSAMPCENTROID    = 4 << D3DSP_DSTMOD_SHIFT,
+        D3DSPDM_FORCE_DWORD      = 0x7FFFFFFF
+    } D3DSHADER_PARAM_DSTMOD_TYPE;
+    #endif
 #else
     #define COM_NO_WINDOWS_H
     #include <windows.h>
